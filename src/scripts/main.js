e92abb899c8e5c08a607277a525a5f7459b28109
nvhoang2001 <ngoviethoang2424@gmail.com>
2021-06-09 10:57:55 +0700

    add lazy-loading img to homepage

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,34 @@
 // import "../styles/style.scss";
+"use strict";
+
+let imgs = document.querySelectorAll(".lazy-img");
+
+let options = {
+	root: null,
+	rootMargin: "15%",
+	threshold: 0.1,
+};
+
+let obsFn = (entries, observer) => {
+	let [entry] = entries;
+	if (!entry.isIntersecting) {
+		return;
+	}
+
+	entry.target.src = entry.target.dataset.src;
+	entry.target.addEventListener("load", loadImg);
+
+	observer.unobserve(entry.target);
+};
+
+const loadImg = function () {
+	this.classList.remove("lazy-img");
+
+	this.removeEventListener("load", loadImg);
+};
+
+let observer = new IntersectionObserver(obsFn, options);
+
+imgs.forEach((img) => {
+	observer.observe(img);
+});
